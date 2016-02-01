168099dd87284ba8b6115a20de85adea0d197149
Jonathan Abbett <jonathan@act.md>
2016-02-01 14:08:35 -0500

    More reliable initial load.

--- client/client.js
+++ client/client.js
@@ -25,3 +25,45 @@ Template.signature.helpers({
         return this.createdAt.relative();
     }
 });
+
+Meteor.startup(function() {
+
+    skel
+        .breakpoints({
+            xlarge: '(max-width: 1680px)',
+            large:  '(max-width: 1280px)',
+            medium: '(max-width: 980px)',
+            small:  '(max-width: 736px)',
+            xsmall: '(max-width: 480px)'
+        });
+
+    var $body = $('body'),
+        $header = $('#header');
+
+    // Mobile?
+    if (skel.vars.mobile)
+        $body.addClass('is-mobile');
+    else
+        skel
+            .on('-medium !medium', function() {
+                $body.removeClass('is-mobile');
+            })
+            .on('+medium', function() {
+                $body.addClass('is-mobile');
+            });
+
+    // Prioritize "important" elements on medium.
+    skel.on('+medium -medium', function() {
+        $.prioritize(
+            '.important\\28 medium\\29',
+            skel.breakpoint('medium').active
+        );
+    });
+
+    $('.scrolly')
+        .scrolly({
+            speed:  1500,
+            offset: $header.outerHeight()
+        });
+
+});
